ed5459dd2ad94cab597d05a93f507ff81006b4e5
xinyong.w@gmail.com <xinyong.w@gmail.com>
2022-09-07 01:34:11 +0800

    config csvclean interface

--- cmd/csvclean/csvclean.go
+++ cmd/csvclean/csvclean.go
@@ -1,9 +1,22 @@
 package csvclean
 
-import "github.com/urfave/cli/v2"
+import (
+	"scsv/cmd"
+
+	"github.com/urfave/cli/v2"
+)
 
 var CSVCleanCMD = &cli.Command{
 	Name:    "csvclean",
 	Aliases: []string{"clean"},
 	Usage:   "修复CSV文件中的常见错误",
+}
+
+func init() {
+	CSVCleanCMD.Flags = append(CSVCleanCMD.Flags, cmd.PublicFlags...)
+	CSVCleanCMD.Flags = append(CSVCleanCMD.Flags,
+		&cli.BoolFlag{Name: "dry-run", Aliases: []string{"n"},
+			Usage: "不创建输出文件,结果信息将会打印到标准错误(STDERR)",
+		},
+	)
 }
