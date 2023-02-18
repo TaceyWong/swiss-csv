27d96c7f54b6da3552e8b4e8ddb5d5b9da4560fb
xinyong.w@gmail.com <xinyong.w@gmail.com>
2023-02-18 20:09:01 +0800

    change interface

--- cmd/csvjoin/csvjoin.go
+++ cmd/csvjoin/csvjoin.go
@@ -1,9 +1,13 @@
 package csvjoin
 
-import "github.com/urfave/cli/v2"
+import (
+	cs "github.com/mitchellh/colorstring"
+	"github.com/urfave/cli/v2"
+)
 
 var CSVJoinCMD = &cli.Command{
-	Name:    "csvjoin",
-	Aliases: []string{"join"},
-	Usage:   "执行类似sql的Join来合并指定列上的CSV文件",
+	Name:        "csvjoin",
+	Aliases:     []string{"join"},
+	Usage:       "执行类似sql的Join来合并指定列上的CSV文件",
+	Description: cs.Color("[light_red]注意：Join操作需要将所有文件读到内存，！！不要在非常大的文件上执行此操作！！[reset]"),
 }
